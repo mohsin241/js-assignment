3b22f8898c7fbb9a2bddf0b554726e60fd753d4e
mohsin241 <68252488+mohsin241@users.noreply.github.com>
2020-07-16 12:14:56 -0700

    Add files via upload

---
+++ js day-2 assignment-2 (script).js
@@ -0,0 +1,26 @@
+let list=["jonas","martha","agnes","dorente"];
+ let list2=["berlin","moscow","nairobi"]
+ let crossover=list.concat(list2)
+ console.log(crossover)
+
+ let leads=list.slice(0,2)
+ console.log(leads)
+
+ list.push("bartos");
+ console.log(list)
+
+   list.pop();
+ console.log(list)
+
+ list.shift();
+ console.log(list)
+
+ list.reverse()
+ console.log(list)
+
+ let str="dark is better than money heist"
+ let str1=str.replace("better","not better")
+ console.log(str1)
+
+ let str2=str.substring(0,4)
+ console.log(str2)
